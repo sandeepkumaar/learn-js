fd4dc78c9ee37c4b4eff5589b2b4fc151a553653
sandeepkumaar.c <cskumaar1992@gmail.com>
2024-06-17 00:32:34 +0530

    double

---
+++ advanced-datastructures/02_linked-list/double-linkedlist.js
@@ -0,0 +1,190 @@
+
+const proto = {
+  search(value) {
+    let index = 0;
+    let compare = this.compare;
+    for (let node of this) {
+      if(compare(node.value, value) == 0) {
+        return index;
+      };
+      index++;
+    }
+    return null;
+  },
+  at(index) {
+    if(index < 0) index = index + this.size;
+    let currentIndex = 0;
+    for(let node of this) {
+      if(index === currentIndex) {
+        return node;
+      }
+      currentIndex++;
+    };
+    return null;
+  },
+
+  // prepend
+  unshift(value) {
+    let node = createNode(value, this.head);
+    this.head = node;
+    if(this.tail == null) {
+      this.tail = node;
+    };
+    this.size++;
+    return node;
+  },
+  // append
+  push(value) {
+    if(this.head == null) {
+      return this.unshift(value);
+    };
+    const node = createNode(value, null)
+    this.tail.next = node;
+    this.tail = node;
+    this.size++;
+    return node;
+  },
+
+  insert(value, index) {
+    // unshift
+    if(this.head === null || index == 0) {
+      return this.unshift(value);
+    }
+    // push
+    if(index == undefined || index > this.size) {
+      return this.push(value);
+    };
+
+    // insert at 
+    let node = createNode(value);
+    let prevNode = this.at(index - 1)
+    let currentNode = prevNode.next;
+    prevNode.next = node;
+    node.next = currentNode;
+    this.size++;
+    return node;
+  },
+  shift() {
+    if(this.head == null) return null;
+    let node = this.head;
+    this.head = this.head.next;
+    this.size--;
+    return node;
+  },
+  pop() {
+    if(this.tail == null) return null;
+    let prevNode = this.at(this.size - 2)
+    let currentNode = prevNode.next;
+    prevNode.next = null
+    this.size--;
+    return currentNode;
+  },
+  delete(value) {
+    let compare = this.compare;
+    if(this.head == null) return null;
+    // shift
+    if(compare(this.head.value, value) == 0) return this.shift(); 
+    // pop
+    if(compare(this.tail.value, value) == 0) return this.pop();
+
+    let prevNode = null
+    let currentNode = null;
+    for(let node of this) {
+      if(compare(node.value, value) == 0) {
+        currentNode = node;
+        break;
+      };
+      prevNode = node;
+    };
+
+    //// delete any
+    prevNode.next = currentNode.next;
+    this.size--;
+    return currentNode;
+  },
+  reverse() {
+    if(this.head == null) return this.head;
+    let prevNode = null;
+    let currentNode = this.head;
+    for(let node of this) {
+      currentNode = node;
+      currentNode.next = prevNode;
+      prevNode = currentNode;
+    };
+    // swap
+    let head = this.head;
+    this.head = this.tail;
+    this.tail = head;
+    return this;
+  },
+  reverseIterator() {
+    return {
+      ...this,
+      *[Symbol.iterator]() {
+        let currentNode = this.tail;
+        while(currentNode !== null) {
+          let value = currentNode;
+          currentNode = currentNode.prev;
+          yield value;
+        }
+      }
+    }
+  },
+  *[Symbol.iterator]() {
+    let currentNode = this.head;
+    while(currentNode !== null) {
+      let value = currentNode;
+      currentNode = currentNode.next;
+      yield value;
+    }
+  }
+};
+
+export function createNode(value, prev=null, next=null) {
+  return {
+    value,
+    next,
+    prev,
+  }
+}
+
+
+export default function createLinkedList(arr=[], compare=(a, b) => a - b) {
+  const {head, tail, size} = arr.reduce((acc, item) => {
+    if(acc.head === null && acc.tail == null) {
+      acc.head = createNode(item, null)
+      acc.tail = acc.head;
+    } else {
+      let node = createNode(item, null);
+      acc.tail.next = node;
+      node.prev = acc.tail;
+      acc.tail = node;
+    }
+    acc.size++;
+    return acc;
+  }, {
+    head: null,
+    tail: null,
+    size: 0,
+    compare,
+  })
+
+  return Object.assign(
+    Object.create(proto),
+    {
+      head,
+      tail,
+      size,
+      compare,
+    }
+  )
+
+};
+
+let linkedList = createLinkedList([1, 2, 3]);
+
+for (let node of linkedList) {
+  console.log(node.value);
+}
+
+
---
+++ advanced-datastructures/02_linked-list/double-linkedlist.test.js
@@ -0,0 +1,180 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+import createLinkedList from './double-linkedlist.js';
+
+let toArray = (linkedList) => {
+  let arr = [];
+  for(let node of linkedList) {
+    arr.push(node.value)
+  };
+  return arr;
+
+}
+
+let compare = (a='', b='') => a.localeCompare(b)
+
+
+test('iterable', async(t) => {
+  await t.test('iterate forward', () => {
+    let linkedList = createLinkedList(['a', 'b', 'c'], compare);
+    assert.deepEqual(linkedList.size, 3);
+    let arr = toArray(linkedList);
+    assert.deepEqual(arr, ['a', 'b', 'c'])
+  })
+  await t.test('iterate reverse', () => {
+    let linkedList = createLinkedList(['a', 'b', 'c'], compare);
+    let arr = toArray(linkedList.reverse());
+    assert.deepEqual(arr, ['c', 'b', 'a'])
+  });
+})
+test('search', () => {
+  let linkedList = createLinkedList(['a', 'b', 'c'], compare);
+  let index = linkedList.search('b');
+  assert.equal(index, 1);
+})
+
+
+test('at', () => {
+  let linkedList = createLinkedList(['a', 'b', 'c']);
+  assert.equal(linkedList.at(2).value, 'c')
+  assert.equal(linkedList.at(3)?.value, undefined)
+  assert.equal(linkedList.at(-1)?.value, 'c')
+});
+
+/*
+test('unshift-shift', async t => {
+  await t.test('unshift', () => {
+    // no head
+    let linkedList = createLinkedList();
+    const node = linkedList.unshift('a');
+    assert.equal(node.value, 'a');
+    assert.equal(linkedList.size, 1);
+    // unshift
+    linkedList.unshift('b');
+    let arr = toArray(linkedList);
+    assert.deepEqual(arr, ['b', 'a']);
+    assert.equal(linkedList.size, 2);
+  })
+  await t.test('shift', () => {
+    let linkedList = createLinkedList();
+    linkedList.unshift('a');
+    let node = linkedList.shift('a');
+    assert.equal(node.value, 'a');
+    assert.equal(linkedList.size, 0);
+  })
+
+})
+
+test('insert', async (t) => {
+  await t.test('no head', () => {
+    let linkedList = createLinkedList();
+    linkedList.insert('c');
+    assert.equal(linkedList.size, 1)
+    assert.equal(linkedList.at(0)?.value, 'c')
+  })
+  await t.test('preprend', () => {
+    let linkedList = createLinkedList(['a']);
+    linkedList.insert('c', 0);
+    assert.equal(linkedList.size, 2)
+    assert.deepEqual(toArray(linkedList), ['c', 'a'])
+  })
+  await t.test('append', () => {
+    let linkedList = createLinkedList();
+    linkedList.insert('a');
+    assert.equal(linkedList.size, 1);
+    assert.equal(linkedList.at(0)?.value, 'a');
+    linkedList.insert('b', 100);
+    assert.equal(linkedList.size, 2);
+    assert.equal(linkedList.at(1)?.value, 'b');
+
+  })
+
+  await t.test('insertAt', () => {
+    let linkedList = createLinkedList(['a', 'b']);
+    linkedList.insert('c', 1);
+    assert.deepEqual(toArray(linkedList), ['a', 'c', 'b']);
+  })
+});
+
+test('delete', async(t) => {
+  await t.test('shift', () => {
+    let linkedList = createLinkedList([], compare);
+    let node = linkedList.delete('c');
+    assert.deepEqual(node, null)
+
+    linkedList.insert('a');
+    linkedList.insert('b');
+    linkedList.insert('c');
+    let node1 = linkedList.delete('a');
+    assert.deepEqual(node1.value, 'a');
+    assert.deepEqual(linkedList.size, 2);
+    assert.deepEqual(toArray(linkedList), ['b', 'c']);
+
+  })
+  await t.test('pop', () => {
+    let linkedList = createLinkedList(['a','b', 'c'], compare);
+    let node2 = linkedList.delete('c');
+    assert.deepEqual(node2.value, 'c');
+    assert.deepEqual(linkedList.size, 2);
+    assert.deepEqual(toArray(linkedList), ['a', 'b']);
+  })
+  await t.test('deleteAny', () => {
+    let linkedList = createLinkedList(['a','b', 'c'], compare);
+    let node2 = linkedList.delete('b');
+    assert.deepEqual(node2.value, 'b');
+    assert.deepEqual(linkedList.size, 2);
+    assert.deepEqual(toArray(linkedList), ['a', 'c']);
+  })
+})
+
+
+test('push-pop', async (t) => {
+  await t.test('push', () => {
+    let linkedList = createLinkedList();
+    let node = linkedList.push('a')
+    assert.equal(node.value, 'a');
+    linkedList.push('b')
+    linkedList.push('c')
+    assert.equal(linkedList.size, 3);
+    let arr = toArray(linkedList);
+    assert.deepEqual(arr, ['a', 'b', 'c']);
+  });
+  await t.test('pop', () => {
+    let linkedList = createLinkedList();
+    // 
+    let value = linkedList.pop();
+    assert.equal(value, null);
+    // 
+    linkedList.push('a');
+    linkedList.push('b');
+    let node = linkedList.pop();
+    assert.equal(node.value, 'b');
+    assert.equal(linkedList.size, 1);
+    // 
+    let arr = toArray(linkedList);
+    assert.deepEqual(arr, ['a']);
+  })
+});
+
+test('reverse', async(t) => {
+  await t.test('null head', () => {
+    let linkedList = createLinkedList();
+    let reverse = linkedList.reverse();
+    assert.deepEqual(reverse, null);
+  })
+
+  await t.test('one value', () => {
+    let linkedList = createLinkedList(['a']);
+    let reverse = linkedList.reverse();
+    assert.deepEqual(toArray(reverse), ['a']);
+  })
+  await t.test('many', () => {
+    let linkedList = createLinkedList(['a', 'b', 'c']);
+    let reverse = linkedList.reverse();
+    assert.deepEqual(toArray(reverse), ['c', 'b', 'a']);
+    assert.deepEqual(toArray(linkedList), ['c', 'b', 'a']);
+  })
+})
+*/
+
+
